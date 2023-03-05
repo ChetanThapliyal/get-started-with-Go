2d6d30ab4d5dad37f9bfb730d73df4ca070813b8
Chetan Thapliyal <chetan_thapliyal@tuta.io>
2023-03-05 17:32:53 +0530

    Different dataTypes in go with Zero values.

--- 2.datatypes.go
+++ 2.datatypes.go
@@ -6,7 +6,7 @@ func main() {
 	// bool
 	var flag bool = true
 	var zeroFlag bool
-	fmt.Printf("bool: %v, zero value: %v\n", flag, zeroFlag)
+	fmt.Printf("bool: %v, zero value: %v\n", flag, zeroFlag)  //bool: true, zero value: false
 
 	// numeric types
 	var i8 int8 = -128
@@ -22,43 +22,39 @@ func main() {
 	var c64 complex64 = 1 + 2i
 	var c128 complex128 = 3 + 4i
 
-	fmt.Printf("int8: %v\n", i8)
-	fmt.Printf("int16: %v\n", i16)
-	fmt.Printf("int32: %v\n", i32)
-	fmt.Printf("int64: %v\n", i64)
-	fmt.Printf("uint8: %v\n", u8)
-	fmt.Printf("uint16: %v\n", u16)
-	fmt.Printf("uint32: %v\n", u32)
-	fmt.Printf("uint64: %v\n", u64)
-	fmt.Printf("float32: %v\n", f32)
-	fmt.Printf("float64: %v\n", f64)
-	fmt.Printf("complex64: %v\n", c64)
-	fmt.Printf("complex128: %v\n", c128)
+	fmt.Printf("int8: %v\n", i8)  //int8: -128
+	fmt.Printf("int16: %v\n", i16) //int16: -32768
+	fmt.Printf("int32: %v\n", i32) //int32: -2147483648
+	fmt.Printf("int64: %v\n", i64)  //int64: -9223372036854775808
+	fmt.Printf("uint8: %v\n", u8)  //uint8: 255
+	fmt.Printf("uint16: %v\n", u16)  //uint16: 65535
+	fmt.Printf("uint32: %v\n", u32)  //uint32: 4294967295
+	fmt.Printf("uint64: %v\n", u64)  //uint64: 18446744073709551615
+	fmt.Printf("float32: %v\n", f32)  //float32: 3.14
+	fmt.Printf("float64: %v\n", f64)  //float64: 3.141592653589793
+	fmt.Printf("complex64: %v\n", c64)  //complex64: (1+2i)
+	fmt.Printf("complex128: %v\n", c128)  //complex128: (3+4i)
 
 	// string
 	var str string = "Hello, World!"
 	var emptyStr string
-	fmt.Printf("string: %v, zero value: %v\n", str, emptyStr)
+	fmt.Printf("string: %v, zero value: %v\n", str, emptyStr)  //string: Hello, World!, zero value:
 
 	// arrays
 	var arr [3]int = [3]int{1, 2, 3}
 	var zeroArr [3]int
-	fmt.Printf("array: %v, zero value: %v\n", arr, zeroArr)
+	fmt.Printf("array: %v, zero value: %v\n", arr, zeroArr)  //array: [1 2 3], zero value: [0 0 0]
 
 	// slices
 	var slice []int = []int{1, 2, 3}
 	var emptySlice []int
-	fmt.Printf("slice: %v, zero value: %v\n", slice, emptySlice)
+	fmt.Printf("slice: %v, zero value: %v\n", slice, emptySlice)  //slice: [1 2 3], zero value: []
 
 	// maps
 	var m map[string]int = map[string]int{"one": 1, "two": 2, "three": 3}
 	var emptyMap map[string]int
-	fmt.Printf("map: %v, zero value: %v\n", m, emptyMap)
+	fmt.Printf("map: %v, zero value: %v\n", m, emptyMap)  //map: map[one:1 three:3 two:2], zero value: map[]
 
-	// pointers
-	var ptr *int = &i32
-	var nilPtr *int
-	fmt.Printf("pointer: %v, zero value: %v\n", ptr, nilPtr)
 
 	// structs
 	type person struct {
@@ -67,6 +63,7 @@ func main() {
 	}
 	var p person = person{"John", 30}
 	var emptyPerson person
-	fmt.Printf("struct: %v, zero value: %v\n", p, emptyPerson)
+	fmt.Printf("struct: %v, zero value: %v\n", p, emptyPerson)  //struct: {John 30}, zero value: { 0}
+
 }
 
