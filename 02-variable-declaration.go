db2a9a3c0d976c7f52bd6e9c181655a1c0664c5e
Chetan Thapliyal <chetan_thapliyal@tuta.io>
2023-03-06 08:06:48 +0530

    Different ways of declaring variables

---
+++ 02-variable-declaration.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//using var keyword
+	// var variable_name data_type = value
+	var age int = 26
+
+	//shorthand method
+	name := "John Doe"
+
+	//var block
+	var (
+		company string = "Google"
+		salary  int    = 26000
+	)
+	
+
+}
+
