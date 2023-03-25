18f36536ca0f3bd2f1922d0f30e41ba1a1d8c1b0
Chetan Thapliyal <chetan_thapliyal@tuta.io>
2023-03-25 11:31:10 +0530

    Modified Solution

--- go-exorcism-solutions/05-Strings&Strings-Package/Welcome-To-Tech-Palace.go
+++ go-exorcism-solutions/05-Strings&Strings-Package/Welcome-To-Tech-Palace.go
@@ -1,6 +1,10 @@
-//package main
-package techpalace
-import "strings" //fmt
+package main
+//package techpalace
+
+import (
+	"fmt"
+	"strings"
+)
 
 // WelcomeMessage returns a welcome message for the customer.
 func WelcomeMessage(customer string) string {
@@ -10,27 +14,22 @@ func WelcomeMessage(customer string) string {
 // AddBorder adds a border to a welcome message.
 func AddBorder(welcomeMsg string, numStarsPerLine int) string {
 	var stars = strings.Repeat("*", numStarsPerLine)
-    return stars + "\n" + welcomeMsg + "\n" + stars
+	return stars + "\n" + welcomeMsg + "\n" + stars
 }
 
 // CleanupMessage cleans up an old marketing message.
 func CleanupMessage(oldMsg string) string {
-	var msg = strings.Split(oldMsg, "\n")[1]
-    var newMsg = strings.Trim(msg, "* ")
-    return newMsg
-}
-
-/*
-CleanupMessage function above is returning empty value if printed, use below code for better results
-
-func CleanupMessage(oldMsg string) string {
-	oldMsg = strings.TrimSpace(oldMsg)
-	var msg = strings.Split(oldMsg, "\n")[1]
-	var newMsg = strings.TrimFunc(msg, func(r rune) bool {
-		return r == '*' || unicode.IsSpace(r)
-	})
+	var newMsg = strings.TrimSpace(strings.ReplaceAll(oldMsg, "*", ""))
 	return newMsg
+
 }
 
-In this case, the function checks for the presence of asterisks or whitespace characters using r == '*' || unicode.IsSpace(r), and trims them from the start and end of the string.
-*/
+func main() {
+	message := `
+	**************************
+	*    BUY NOW, SAVE 10%   *
+	**************************
+	`
+	var x string = CleanupMessage(message)
+	fmt.Println(x)
+}
