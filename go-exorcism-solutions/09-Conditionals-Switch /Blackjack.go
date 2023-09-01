190493c6543df8799ad191782fd26a52f712c2aa
Chetan Thapliyal <chetan_thapliyal@tuta.io>
2023-09-01 13:06:17 +0530

    Card values

--- go-exorcism-solutions/09-Conditionals-Switch /Blackjack.go
+++ go-exorcism-solutions/09-Conditionals-Switch /Blackjack.go
@@ -1,5 +1,21 @@
 package blackjack
 
+var cardValues = map[string]int{
+	"ace":   11,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+	"ten":   10,
+	"jack":  10,
+	"queen": 10,
+	"king":  10,
+}
+
 // ParseCard returns the integer value of a card following blackjack ruleset.
 func ParseCard(card string) int {
 	panic("Please implement the ParseCard function")
