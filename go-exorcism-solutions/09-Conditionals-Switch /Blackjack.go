8ce01e3759aa983c1feb27d06f3dfc81751f8b46
Chetan Thapliyal <chetan_thapliyal@tuta.io>
2023-09-01 12:14:25 +0530

    Basic Structure of Solution

---
+++ go-exorcism-solutions/09-Conditionals-Switch /Blackjack.go
@@ -0,0 +1,12 @@
+package blackjack
+
+// ParseCard returns the integer value of a card following blackjack ruleset.
+func ParseCard(card string) int {
+	panic("Please implement the ParseCard function")
+}
+
+// FirstTurn returns the decision for the first turn, given two cards of the
+// player and one card of the dealer.
+func FirstTurn(card1, card2, dealerCard string) string {
+	panic("Please implement the FirstTurn function")
+}
