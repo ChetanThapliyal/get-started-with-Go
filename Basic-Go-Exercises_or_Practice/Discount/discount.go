1bf3f6ef5eca595e49ffa523843d3d3c38d4f813
Chetan Thapliyal <chetan_thapliyal@tuta.io>
2023-11-26 17:14:34 +0530

    basic structure of solution

---
+++ Basic-Go-Exercises_or_Practice/Discount/discount.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func discountedPrice(product string, price float64) float64 {
+	var discount float64
+
+}
+
+func main() {
+	fmt.Println(discountedPrice("apples", 100))
+	fmt.Println(discountedPrice("orange", 100))
+	fmt.Println(discountedPrice("bananas", 100))
+	fmt.Println(discountedPrice("bananas", 100))
+	fmt.Println(discountedPrice("oranges", 100))
+}
