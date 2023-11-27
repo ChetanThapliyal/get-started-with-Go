806aab8cc803bea02e7b1d359077c9b422726b80
Chetan Thapliyal <chetan_thapliyal@tuta.io>
2023-11-27 17:30:16 +0530

    wordFrequency func added

--- Basic-Go-Exercises_or_Practice/WordFrequency/frequency.go
+++ Basic-Go-Exercises_or_Practice/WordFrequency/frequency.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"strings"
+)
+
+func wordFrequency(text string) map[string]int {
+	words := strings.Fields(text)
+	frequency := make(map[string]int)
+	for _, word := range words {
+		frequency[word]++
+	}
+	return frequency
+}
